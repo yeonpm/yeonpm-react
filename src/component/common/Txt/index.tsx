1919f6a683d4a4a492c3fbf3026a27eeeabb5765
hs770616 <hs770616@naver.com>
2024-12-10 18:02:06 +0900

    feat: add className Txt

--- src/component/common/Txt/index.tsx
+++ src/component/common/Txt/index.tsx
@@ -6,33 +6,25 @@ import styled from "@emotion/styled";
 import { resolveProps } from "react-style-props";
 import { TxtProps } from "./type";
 
-const defaultColor = "#2A333C";
-
 const Txt = forwardRef(
   (
     { span, children, ...props }: TxtProps,
     ref?: Ref<HTMLDivElement>
   ): ReactElement => {
     const { styleString, otherProps } = resolveProps(props as any);
+    const StyledTxt = span ? StyledTxtSpan : StyledTxtP;
 
-    return span ? (
-      <StyledTxtSpan
+    return (
+      <StyledTxt
         ref={ref}
-        color={defaultColor}
         css={styleString}
         {...otherProps}
+        className={`yeonpm-txt${
+          otherProps.className ? ` ${otherProps.className}` : ""
+        }`}
       >
         {children}
-      </StyledTxtSpan>
-    ) : (
-      <StyledTxtP
-        ref={ref}
-        color={defaultColor}
-        css={styleString}
-        {...otherProps}
-      >
-        {children}
-      </StyledTxtP>
+      </StyledTxt>
     );
   }
 );
@@ -40,15 +32,9 @@ const Txt = forwardRef(
 export default memo(Txt);
 
 const StyledTxtP = styled("p")<{ css?: string }>`
-  margin: 0;
-  color: ${defaultColor};
-  font-family: Pretendard Variable;
   ${({ css }) => css}
 `;
 
 const StyledTxtSpan = styled("span")<{ css?: string }>`
-  margin: 0;
-  color: ${defaultColor};
-  font-family: Pretendard Variable;
   ${({ css }) => css}
 `;
