a074be780499addabc5b787ff3af36b45144a4e6
hs770616 <hs770616@naver.com>
2024-12-10 17:08:19 +0900

    fix: remove export NextCacheProvider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as NextCacheProvider } from "./component/NextCacheProvider";
+// export { default as NextCacheProvider } from "./component/NextCacheProvider";
 
 export { default as Wrapper } from "./component/common/Wrapper";
 export type { WrapperProps } from "./component/common/Wrapper/type";
